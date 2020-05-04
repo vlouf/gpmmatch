34c039a03da98c308e1e753d57b131610e96b88f
Valentin Louf <valentin.louf@bom.gov.au>
2020-05-04 14:00:11 +1000

    MODIF: Offset is median not mean

--- gpmmatch/gpmmatch.py
+++ gpmmatch/gpmmatch.py
@@ -66,9 +66,9 @@ def get_offset(matchset):
     std_refl_gr = matchset.std_refl_gr.values
 
     delta_std = np.abs(std_refl_gpm - std_refl_gr)
-    pos = (delta_std < 1) & (refl_gr >= 24) & (refl_gr <= 36)
+    pos = (delta_std < 1) & (refl_gr >= 24) & (refl_gr <= 36) & (~np.isnan(refl_gpm)) & (~np.isnan(refl_gr))
 
-    offset = np.nanmean(refl_gr[pos] - refl_gpm[pos])
+    offset = np.median(refl_gr[pos] - refl_gpm[pos])
     return offset
 
 
