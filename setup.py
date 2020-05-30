a6476c3d0c89ce99e19e52d3fcb72e66cace37a5
Valentin Louf <valentin.louf@bom.gov.au>
2020-05-30 15:47:15 +1000

    MODIF: pep8 formatting

--- setup.py
+++ setup.py
@@ -8,15 +8,22 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'gpmmatch'
-DESCRIPTION = 'Volume matching of ground radar and GPM satellite data.'
-URL = 'https://github.com/vlouf/gpmmatch'
-EMAIL = 'valentin.louf@bom.gov.au'
-AUTHOR = 'Valentin Louf'
+NAME = "gpmmatch"
+DESCRIPTION = "Volume matching of ground radar and GPM satellite data."
+URL = "https://github.com/vlouf/gpmmatch"
+EMAIL = "valentin.louf@bom.gov.au"
+AUTHOR = "Valentin Louf"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "arm_pyart", "numpy", "netCDF4", "h5py", "pyproj", 'xarray', 'pandas', 'scipy'
+    "arm_pyart",
+    "numpy",
+    "netCDF4",
+    "h5py",
+    "pyproj",
+    "xarray",
+    "pandas",
+    "scipy",
 ]
 
 # The rest you shouldn't have to touch too much :)
@@ -28,19 +35,20 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
 
 class PublishCommand(Command):
     """Support setup.py publish."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -50,16 +58,16 @@ class PublishCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except FileNotFoundError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
@@ -67,28 +75,26 @@ class PublishCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version='1.0.0',
+    version="1.0.0",
     description=DESCRIPTION,
     long_description=long_description,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests', 'example', 'notebooks')),
-    package_data={'gpmmatch': ['data/radar_site_list.csv']},
+    packages=find_packages(exclude=("tests", "example", "notebooks")),
+    package_data={"gpmmatch": ["data/radar_site_list.csv"]},
     install_requires=REQUIRED,
     include_package_data=True,
-    license='ISC',
+    license="ISC",
     classifiers=[
         # Trove classifiers
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'publish': PublishCommand,
-    },
+    cmdclass={"publish": PublishCommand,},
 )
