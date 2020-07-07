819617aabc0a4c789d0246a3a69efa6bbff12ab2
Valentin Louf <valentin.louf@bom.gov.au>
2020-07-07 10:35:35 +1000

    MODIF: rem obsolete function

--- gpmmatch/correct.py
+++ gpmmatch/correct.py
@@ -5,7 +5,7 @@ Various utilities for correction and conversion of satellite data.
 @author: Valentin Louf <valentin.louf@bom.gov.au>
 @institutions: Monash University and the Australian Bureau of Meteorology
 @creation: 17/02/2020
-@date: 06/07/2020
+@date: 07/07/2020
     correct_xband_attenuation
     correct_cband_attenuation
     correct_parallax
@@ -234,60 +234,6 @@ def get_offset(matchset, dr, nbins=200) -> float:
     maxpos = np.argmax(area)
     gr_offset = offset[maxpos]
     return gr_offset
-
-
-# def get_offset(matchset, dr) -> float:
-#     '''
-#     Compute the Offset between GR and GPM. It will try to compute the mode of
-#     the distribution and if it fails, then it will use the mean.
-
-#     Parameter:
-#     ==========
-#     matchset: xr.Dataset
-#         Dataset of volume matching.
-#     dr: int
-#         Ground radar gate spacing (m).
-
-#     Returns:
-#     ========
-#     offset: float
-#         Offset between GR and GPM
-#     '''
-#     refl_gpm = matchset.refl_gpm_grband.values
-#     refl_gr = matchset.refl_gr_weigthed.values
-#     std_refl_gpm = matchset.std_refl_gpm.values
-#     std_refl_gr = matchset.std_refl_gr.values
-#     sample_gr = matchset['sample_gr'].values
-
-#     dr_thld = (25, 90)
-#     if dr == 500:
-#         dr_thld = (10, 90)
-#     elif dr == 1000:
-#         dr_thld = (5, 90)
-
-#     pos = ((std_refl_gpm > 0.2) & # (std_refl_gpm < 5) &
-#            (std_refl_gr > 0) & # (std_refl_gr < 5) &
-#            (sample_gr > dr_thld[0]) &
-#            (sample_gr < dr_thld[1]) &
-#            (np.abs(refl_gpm - refl_gr) < 15) &
-#            (~np.isnan(refl_gpm)) &
-#            (~np.isnan(refl_gr)) &
-#            (refl_gr >= 21) &
-#            (refl_gr <= 36))
-
-#     x1 = refl_gpm[pos]
-#     x2 = refl_gr[pos]
-#     deltax = (x2 - x1)
-#     deltax = deltax[~np.isnan(deltax)]
-
-#     m, _ = mode(np.round(deltax * 2) / 2, nan_policy='omit')
-#     try:
-#         npos = ((deltax < m[0] + deltax.std()) & (deltax > m[0] - deltax.std()))
-#         offset = deltax[npos].mean()
-#     except Exception:
-#         offset = np.mean(deltax)
-
-#     return offset
 
 
 def grid_displacement(field1, field2):
