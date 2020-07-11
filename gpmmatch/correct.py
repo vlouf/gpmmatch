508f05463cd3ea2aa0766a35e59fc62a2081f3ab
Valentin Louf <valentin.louf@bom.gov.au>
2020-07-11 20:03:57 +1000

    MODIF: change offset comments

--- gpmmatch/correct.py
+++ gpmmatch/correct.py
@@ -5,7 +5,7 @@ Various utilities for correction and conversion of satellite data.
 @author: Valentin Louf <valentin.louf@bom.gov.au>
 @institutions: Monash University and the Australian Bureau of Meteorology
 @creation: 17/02/2020
-@date: 10/07/2020
+@date: 11/07/2020
     correct_attenuation
     correct_parallax
     correct_refraction
@@ -204,13 +204,16 @@ def get_offset(matchset, dr, nbins=200) -> float:
     offset: float
         Offset between GR and GPM
     '''
+    offset = np.arange(-15, 15, .2, dtype=np.float64)
+    area = np.zeros_like(offset)
+
     refl_gpm = matchset.refl_gpm_grband.values.flatten().copy()
     refl_gr = matchset.refl_gr_weigthed.values.flatten().copy()
+
     pos = (refl_gpm > 36) | (refl_gr > 36)
     refl_gpm[pos] = np.NaN
-    refl_gr[pos] = np.NaN
-    offset = np.arange(-15, 15, .2)
-    area = np.zeros_like(offset)
+    refl_gr[pos] = np.NaN    
+    
     pdf_gpm, _ = np.histogram(refl_gpm, range=[0, 50], bins=nbins, density=True)
     for idx, a in enumerate(offset):
         pdf_gr, _ = np.histogram(refl_gr  - a, range=[0, 50], bins=nbins, density=True)
