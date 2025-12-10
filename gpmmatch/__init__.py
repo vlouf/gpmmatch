2babf1c5fc05ad81708de3c6e20277a8c41e1815
Valentin Louf <valentin.louf@bom.gov.au>
2025-12-10 15:44:52 +1100

    refactor:proper __init__ file

--- gpmmatch/__init__.py
+++ gpmmatch/__init__.py
@@ -1,6 +1,35 @@
-# Import functions
-from .gpmmatch import volume_matching
-from .gpmmatch import vmatch_multi_pass
+"""
+GPM volume matching package.
 
-# Import error class.
-from .gpmmatch import NoRainError
+Volume matching of ground radar and GPM satellite data.
+"""
+
+from .gpmmatch import (
+    NoRainError,
+    generate_filename,
+    get_radar_coordinates,
+    get_gr_reflectivity,
+    has_valid_data,
+    volume_matching,
+    vmatch_multi_pass,
+    MIN_SAMPLE_POINTS,
+    MIN_REFL_SAMPLES,
+    MAX_OFFSET_THRESHOLD,
+)
+
+__version__ = "1.4.0"
+__author__ = "Valentin Louf"
+__email__ = "valentin.louf@bom.gov.au"
+
+__all__ = [
+    "NoRainError",
+    "generate_filename",
+    "get_radar_coordinates",
+    "get_gr_reflectivity",
+    "has_valid_data",
+    "volume_matching",
+    "vmatch_multi_pass",
+    "MIN_SAMPLE_POINTS",
+    "MIN_REFL_SAMPLES",
+    "MAX_OFFSET_THRESHOLD",
+]
